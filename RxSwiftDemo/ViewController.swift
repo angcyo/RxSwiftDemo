b3e9238569f0fe35ece2c20f4364d9039ba1f0c1
angcyo <angcyo@126.com>
2016-08-24 23:16:32 -0700

    添加TextField.rx_text demo

--- RxSwiftDemo/ViewController.swift
+++ RxSwiftDemo/ViewController.swift
@@ -12,11 +12,17 @@ import RxCocoa
 
 class ViewController: UIViewController {
 
-	let n = Variable(1)
+	let n = Variable(1) // 可变的变量
+	@IBOutlet weak var textFieldOutlet: UITextField!
+	@IBOutlet weak var buttonOutlet: UIButton!
+
+	var textFieldDisp: Disposable!
 
 	override func viewDidLoad() {
 		super.viewDidLoad()
 		// Do any additional setup after loading the view, typically from a nib.
+
+		testTextField()
 	}
 
 	override func didReceiveMemoryWarning() {
@@ -25,15 +31,44 @@ class ViewController: UIViewController {
 	}
 
 	@IBAction func onButton(sender: AnyObject) {
-		test()
+		// testFunc()
+
+		if textFieldDisp != nil {
+			textFieldDisp.dispose() // 取消订阅
+			textFieldDisp = nil
+		} else {
+			testTextField()
+		}
+
+//		textFieldOutlet.rx_hidden.onNext(true)//隐藏了..
+//		textFieldOutlet.rx_enabled.onNext(false)//不可用了...
+
 	}
 }
 
 extension ViewController {
-	func test() {
+	func testFunc() {
 		n.asObservable().map { int in
 			print("Test \(NSThread.isMainThread()) \(#function) \(int)")
-		}.subscribe()
+		}.subscribe().dispose()
+	}
+
+	func testTextField() {
+		// 订阅TextField的文本的改变
+		// 1:
+//		textFieldDisp = textFieldOutlet.rx_text.subscribe { event in
+//			print("event \(#function) \(event) \(event.element!)")
+//		}
+
+		// 2:
+		textFieldDisp = textFieldOutlet.rx_text.subscribeNext { element in
+			print("event \(#function) \(element)")
+		}
+
+//		textFieldOutlet.rx_selected.on { event in
+//			print("Test \(NSThread.isMainThread()) \(#function) \(event.)")
+//		}
+
 	}
 }
 
